3d5c7404f6b083a50fc9c61bfb6b5f7cab493f72
Ömer Apaydın <omerapaydin90@gmail.com>
2026-06-04 18:15:09 +0300

    pm.wrappedValue.dismiss()

--- SegueSwiftUI/SonucEkrani.swift
+++ SegueSwiftUI/SonucEkrani.swift
@@ -8,10 +8,11 @@
 import SwiftUI
 
 struct SonucEkrani: View {
+    @Environment(\.presentationMode) var pm
     var body: some View {
         VStack{
             Button("Kapat"){
-                
+                pm.wrappedValue.dismiss()
             }
         }
     }

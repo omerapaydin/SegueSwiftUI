8234a65494dbc32304fcea8aeb7cb750d087b950
Ömer Apaydın <omerapaydin90@gmail.com>
2026-06-04 17:41:34 +0300

    NavigationLink

---
+++ SegueSwiftUI/Anasayfa.swift
@@ -0,0 +1,18 @@
+//
+//  Anasayfa.swift
+//  SegueSwiftUI
+//
+//  Created by Ömer Apaydın on 4.06.2026.
+//
+
+import SwiftUI
+
+struct Anasayfa: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Anasayfa()
+}
---
+++ SegueSwiftUI/OyunEkrani.swift
@@ -0,0 +1,18 @@
+//
+//  OyunEkrani.swift
+//  SegueSwiftUI
+//
+//  Created by Ömer Apaydın on 4.06.2026.
+//
+
+import SwiftUI
+
+struct OyunEkrani: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OyunEkrani()
+}

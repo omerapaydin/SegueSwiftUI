a22008f0c87d8ef1fa3fc001a644b5b4428bf6b6
Ömer Apaydın <omerapaydin90@gmail.com>
2026-06-04 17:28:36 +0300

    Initial Commit

---
+++ SegueSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SegueSwiftUI
+//
+//  Created by Ömer Apaydın on 4.06.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SegueSwiftUI/SegueSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SegueSwiftUIApp.swift
+//  SegueSwiftUI
+//
+//  Created by Ömer Apaydın on 4.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct SegueSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

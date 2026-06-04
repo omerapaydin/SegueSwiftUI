68b7f1372a0fb8821e78cf87ab85ef8a10af25ee
Ömer Apaydın <omerapaydin90@gmail.com>
2026-06-04 18:06:00 +0300

    sheet

---
+++ SegueSwiftUI/SonucEkrani.swift
@@ -0,0 +1,18 @@
+//
+//  SonucEkrani.swift
+//  SegueSwiftUI
+//
+//  Created by Ömer Apaydın on 4.06.2026.
+//
+
+import SwiftUI
+
+struct SonucEkrani: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SonucEkrani()
+}

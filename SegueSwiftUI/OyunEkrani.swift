0a005f72fed805d646b655850829167e158a5558
Ömer Apaydın <omerapaydin90@gmail.com>
2026-06-04 17:47:38 +0300

    presentationMode

--- SegueSwiftUI/OyunEkrani.swift
+++ SegueSwiftUI/OyunEkrani.swift
@@ -8,8 +8,16 @@
 import SwiftUI
 
 struct OyunEkrani: View {
+    @Environment(\.presentationMode) var pm
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing:100){
+            Button("Bitti"){
+                
+            }
+            Button("Geri"){
+                pm.wrappedValue.dismiss()
+            }
+        }.navigationTitle("Oyun")
     }
 }
 
